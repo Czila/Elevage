dfdc3f18db494d4c49511e24ebaea73cea117dc6
Czila <lcesar.mk@gmail.com>
2022-06-30 16:09:26 +0200

    connexion mngdb et serveur du back

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const cats = require('./routes/cats.js');
+const app = express();
+app.use(express.json()); 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+app.use('/cats', cats); 
+app.get('/messages', (req, res) => {
+  res.send('this is all of your messages');
+});
+app.listen(3000, () => console.log('Server listenning on port 3000'));
